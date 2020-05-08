157bc54cefe343c8f3827d43ab9bfff0d03d40e4
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-08 14:09:50 -0300

    container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import Inicial from './componentes/paginas/Inicial';
+import GlobalStyle from './styles/global'
 import Navbar from './componentes/navbar/Navbar';
 import Backtend from './componentes/paginas/Backend';
 import Frontend from './componentes/paginas/Frondend';
+import Mobile from './componentes/paginas/mobile/Mobile';
 
 
 function App() {
   return (
     <>
+    <GlobalStyle />
     <Navbar/>
     <Switch>
     {/* <Route path="/" exact component={Inicial} /> */}
     <Route path="/backend" exact component={Backtend} /> 
-    <Route path="/frontend" exact component={Frontend} />                                 
+    <Route path="/frontend" exact component={Frontend} />
+    <Route path="/mobile" exact component={Mobile} />                                      
     </Switch>
     </>
   );
