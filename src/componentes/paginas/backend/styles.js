1fa3ffb259105c35394b9bb08b81fb8a4aff6b4c
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-09 15:45:23 -0300

    estilo backend

---
+++ src/componentes/paginas/backend/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const List = styled.ul`
+    list-style: none;
+    border-bottom: 1px solid #eee;
+    margin-bottom: 30px;
+`;
+
+export const Title = styled.h5`
+    color: #dc3545!important;
+`;
